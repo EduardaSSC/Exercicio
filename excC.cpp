adce7b80dcc9eba9e9af57a807c73e4581a3303d
EduardaSSC <dudaserpacamboim@hotmail.com>
2016-05-24 09:31:36 -0300

    Add files via upload

--- excC.cpp
+++ excC.cpp
@@ -6,7 +6,7 @@ printf("informe o valor pra A");
 scanf("%i",&A);
 printf("informe o valor de B");
 scanf("%f",&B);
-printf("informe o valor de C");
+printf("informe o valor de D");
 scanf("%i",&C);
 B = A+C; 
 printf("o valor de B = %.0f que é a soma de A = %i e c = %i", B, A, C);
